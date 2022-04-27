2ac3f549428d6e9ec5519d96b05a7839f0232635
WoolfiAleks <aleksjavadev@gmail.com>
2022-04-27 14:46:59 +0300

    single logging method with varaible in properties

--- src/main/java/org/daoTaskManager/App.java
+++ src/main/java/org/daoTaskManager/App.java
@@ -1,15 +1,18 @@
 package org.daoTaskManager;
+import org.apache.log4j.Logger;
 import org.daoTaskManager.utils.ClassNameUtil;
 import org.daoTaskManager.utils.ComandExecuter;
 import java.io.IOException;
 
+import static org.daoTaskManager.utils.ClassNameUtil.getCurrentClassName;
+
 public class App 
 {
-//    private static final Logger rootLogger = LoggerFactory.getLogger(App.class);
+     private static final Logger userLogger = Logger.getLogger(getCurrentClassName());
 
     public static void main( String[] args ) throws IOException {
-//        rootLogger.info("Main is starred");
-//        rootLogger.info("+");
+        userLogger.info("Main is starred");
+        userLogger.info("+");
         ComandExecuter.filtrFromArgs(args);
     }
 }
