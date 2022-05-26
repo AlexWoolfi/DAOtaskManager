f8a19d8f9743e103d7d9232e97d0b901780b56d1
WoolfiAleks <aleksjavadev@gmail.com>
2022-05-26 13:19:35 +0300

    Jdbc+hibernate+postgresql

---
+++ src/main/java/org/daoTaskManager/abstractFactory/jdbcDao/JdbcDaoFactory.java
@@ -0,0 +1,10 @@
+package org.daoTaskManager.abstractFactory.jdbcDao;
+
+import org.daoTaskManager.abstractFactory.DaoFactory;
+
+public class JdbcDaoFactory implements DaoFactory {
+    @Override
+    public Object createDaoObject(String s) {
+        return null;
+    }
+}
---
+++ src/main/java/org/daoTaskManager/abstractFactory/jdbcDao/JdbcTaskDaoImpl.java
@@ -0,0 +1,18 @@
+package org.daoTaskManager.abstractFactory.jdbcDao;
+
+import org.daoTaskManager.abstractFactory.TaskDao;
+import org.daoTaskManager.entity.Task;
+
+import java.util.List;
+
+public class JdbcTaskDaoImpl implements TaskDao {
+    @Override
+    public void addTaskToDB(String taskName, String taskBody, long id) {
+
+    }
+
+    @Override
+    public List<Task> showTasksfromUnicUser(String USerunicName) {
+        return null;
+    }
+}
